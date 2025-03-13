d743dadff1fa34dcafad8994f2a91c6a6e4a56cb
orton <ortonhlauli@outlook.com>
2025-03-13 22:36:02 +0200

    testing config

---
+++ src/environments/environment.production.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  pokeDexBaseApiUrl: 'https://fin-man.cloud/sbg-poke/api/pokedex',
+  firebaseConfig: {
+    apiKey: "AIzaSyAVLe8ocmTBMKJzRVRJS6PmduL0okeDLOQ",
+    authDomain: "sbg-pokedex.firebaseapp.com",
+    projectId: "sbg-pokedex",
+    storageBucket: "sbg-pokedex.firebasestorage.app",
+    messagingSenderId: "687762101870",
+    appId: "1:687762101870:web:b88ca510ba53c31d93b87f"
+  }
+};
