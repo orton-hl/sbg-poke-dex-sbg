126d20735c2f01d48d4baf3cac6a72ddeac870b5
orton <ortonhlauli@outlook.com>
2025-03-13 19:10:10 +0200

    clean up

--- src/app/shared/comps/pokemon/pokemon.component.ts
+++ src/app/shared/comps/pokemon/pokemon.component.ts
@@ -1,15 +1,13 @@
-import {Component, Input, input} from '@angular/core';
-import {JsonPipe, NgIf, TitleCasePipe} from '@angular/common';
+import {Component, Input} from '@angular/core';
+import {NgIf, TitleCasePipe} from '@angular/common';
 import {Pokemon} from '../../models/data';
 import {Tag} from 'primeng/tag';
 import {Divider} from 'primeng/divider';
 import {Tooltip} from 'primeng/tooltip';
-import {ImgViewComponent} from '../img-view/img-view.component';
 
 @Component({
   selector: 'app-pokemon',
   imports: [
-    ImgViewComponent,
     Tag,
     Divider,
     NgIf,
